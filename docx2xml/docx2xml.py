9ecf045b73c2e88860190f03761dd23a719f9f6f
tjysdsg <tjy1018543509@163.com>
2019-03-10 22:25:04 +0800

    fix not working correctly when current working directory is not where __file__ is in

--- docx2xml/docx2xml.py
+++ docx2xml/docx2xml.py
@@ -1,19 +1,27 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('oj-template.xml')
-root = tree.getroot()
-
+# parse command line arguments
 import argparse
-
 parser = argparse.ArgumentParser(prog='docx2xml',description='Convert docx to xml for importing problems on DKUOJ')
 parser.add_argument('filename',type=str, action='store', help='docx file to convert')
 
 full_file_name = vars(parser.parse_args())['filename']
 file_name = full_file_name.split('.')[0]
 
+# the directory which contains this file
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+print("Using templates in", dir_path)
+
+# ElementTree for parsing xml template
+import xml.etree.ElementTree as ET
+tree = ET.parse(os.path.join(dir_path,'oj-template.xml'))
+root = tree.getroot()
+
+# python-docx for parsing docx
 import docx
 doc = docx.Document(full_file_name)
 
-
+# content of a problem
 section_content = {"Title":"",
 "Level":"",
 "Description":"",
@@ -27,14 +35,13 @@ section_content = {"Title":"",
 "Source/Category":""
 }
 
+# parse paragraph by paragraph, but some content might contains multiple paragraphs
 for i in range(len(doc.paragraphs)):
     if doc.paragraphs[i].text in section_content.keys():
         t = doc.paragraphs[i].text
     else:
+        # add new line after a paragraph
         section_content[t] += '\n' + doc.paragraphs[i].text
-
-#print(section_content)
-
 
 root[1][0].text = section_content['Title']
 root[1][3].text = section_content['Description']
---
+++ src/template.c
@@ -0,0 +1,15 @@
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        int i = 0;
+        for (; i < n; i++) {
+            int a, b;
+            scanf("%d %d", &a, &b);
+        }
+    }
+    return 0;
+}
