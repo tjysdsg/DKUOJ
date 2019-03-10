1b13ce08965c1534f0d130cd774e205011eae925
tjysdsg <tjy1018543509@163.com>
2019-03-10 20:21:44 +0800

    fix not showing new line, add CLI

--- docx2xml.py
+++ docx2xml.py
@@ -2,12 +2,17 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('oj-template.xml')
 root = tree.getroot()
 
-print(root[1][0].tag)
-print(root[1][3].tag)
-print(root[1][4].tag)
+import argparse
+
+parser = argparse.ArgumentParser(prog='docx2xml',description='Convert docx to xml for importing problems on DKUOJ')
+parser.add_argument('filename',type=str, action='store', help='docx file to convert')
+
+full_file_name = vars(parser.parse_args())['filename']
+file_name = full_file_name.split('.')[0]
 
 import docx
-doc = docx.Document('/home/tjy/Downloads/DKUOJ-UserID-Title.docx')
+doc = docx.Document(full_file_name)
+
 
 section_content = {"Title":"",
 "Level":"",
@@ -26,7 +31,19 @@ for i in range(len(doc.paragraphs)):
     if doc.paragraphs[i].text in section_content.keys():
         t = doc.paragraphs[i].text
     else:
-        section_content[t] += doc.paragraphs[i].text
+        section_content[t] += '\n' + doc.paragraphs[i].text
 
 #print(section_content)
 
+
+root[1][0].text = section_content['Title']
+root[1][3].text = section_content['Description']
+root[1][4].text = section_content['Input']
+root[1][5].text = section_content['Output']
+root[1][6].text = section_content['Sample Input']
+root[1][7].text = section_content['Sample Output']
+root[1][8].text = section_content['Test Input']
+root[1][9].text = section_content['Test Output']
+root[1][10].text = section_content['Hint']
+
+tree.write(file_name + '.xml')
